50cba2fbb4f2fbfdc1549f5aea4958f164cc3af6
Juan0294 <71714305+Juan0294@users.noreply.github.com>
2022-05-16 04:37:51 -0500

    Update Ejercicio2.php

--- Ejercicios Logica/Ejercicio2/Ejercicio2.php
+++ Ejercicios Logica/Ejercicio2/Ejercicio2.php
@@ -1,1 +1,22 @@
 
+<?php 
+
+require_once ('./clases/Comprobar.php');
+
+$cadenas_A_Evaluar = array("Hello World", "2 + 10 / 2 - 20", "(2 + 10) / 2 - 20", "(2 + 10 / 2 - 20");
+
+$objComprobar = new Comprobar(null);
+
+foreach($cadenas_A_Evaluar as $cadena){
+
+    echo $objComprobar->operation((str_replace(" ", "", $cadena)));
+    
+}
+
+foreach($cadenas_A_Evaluar as $cadena){
+
+    echo $objComprobar->compute((str_replace(" ", "", $cadena)));
+    
+}
+
+?>
